8fb2e347a6e8df93c31ee468cbb671a71bdb2527
paridhisehgal <sehgalparidhi7@gmail.com>
2026-07-29 19:45:04 +0530

    Implement basic calculator functionality in main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+num1 = float(input("Enter first number: "))
+op = (input("Enter operator: "))
+num2 = float(input("Enter second number: "))
+
+if op == "+" :
+    print(num1 + num2)
+
+elif op == "-":
+    print (num1 - num2)
+
+elif op == "*":
+    print (num1 * num2)
+
+elif op ++ "/":
+    print (num1/ num2)
+
+
+else:
+    print("Invalid operator")
