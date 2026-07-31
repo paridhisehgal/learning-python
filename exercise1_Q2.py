c568f7013896062734ccde16de4aaec7b7f30d92
paridhisehgal <sehgalparidhi7@gmail.com>
2026-07-31 14:27:00 +0530

    Update exercise1_Q2.py to concatenate a word multiple times based on user input

--- exercise1_Q2.py
+++ exercise1_Q2.py
@@ -1,2 +1,11 @@
-Word = input ("Enter any word: ")
-print (len(Word))
+word = input( "Enter a word: ")
+number = int(input("Enter a number: "))
+result = ""
+
+for i in range (number):
+    if i == 0:
+        result += word
+    else:
+        result += f' {word}'
+
+print("result is: " + result)
