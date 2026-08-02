e3c90053ea0fbf7c5eab4842224298a254df2ab8
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-02 21:16:05 +0530

    Fix syntax error in basic_calculator.py for division operator and clean up formatting

--- basic_calculator.py
+++ basic_calculator.py
@@ -4,16 +4,11 @@ num2 = float(input("Enter second number: "))
 
 if op == "+" :
     print(num1 + num2)
-
 elif op == "-":
     print (num1 - num2)
-
 elif op == "*":
     print (num1 * num2)
-
 elif op ++ "/":
     print (num1/ num2)
-
-
 else:
     print("Invalid operator")
