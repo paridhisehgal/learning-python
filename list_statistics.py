ca5f60bb38229c286630815f84b2a1ebdf896d31
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-04 22:57:21 +0530

    Enhance list_statistics.py to improve user prompts and output formatting for entered numbers and calculated statistics.

--- list_statistics.py
+++ list_statistics.py
@@ -1,16 +1,17 @@
-from math import*
-number =[]
+from math import *
 
-for n in range(0,8):
-    num = float(input("enter a number: "))
+number = []
+
+for n in range(0, 8):
+    num = float(input("enter a number > "))
     number.append(num)
 
-print (number)
-print ("The biggest number:", max(number))
-print ("The lowest number:", min(number))
-print ("The sum of all 8 numbers:", sum(number))
-print ("The average of all 8 numbers:", sum(number)/8)
-print("The ascending order of the list: ", sorted(number))
+print("Your entered the number:", number)
+print("↪ The biggest number:", max(number))
+print("↪ The lowest number:", min(number))
+print("↪ The sum of all 8 numbers:", sum(number))
+print("↪ The average of all 8 numbers:", sum(number)/8)
+print("↪ The ascending order of the list:", sorted(number))
 
 
      
