a374d7c509e124be9376a7ccdff6cd19091e8ad2
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-04 22:52:05 +0530

    Add list_statistics.py to collect user-inputted numbers and calculate statistics including max, min, sum, average, and sorted order.

---
+++ list_statistics.py
@@ -0,0 +1,20 @@
+from math import*
+number =[]
+
+for n in range(0,8):
+    num = float(input("enter a number: "))
+    number.append(num)
+
+print (number)
+print ("The biggest number:", max(number))
+print ("The lowest number:", min(number))
+print ("The sum of all 8 numbers:", sum(number))
+print ("The average of all 8 numbers:", sum(number)/8)
+print("The ascending order of the list: ", sorted(number))
+
+
+     
+    
+
+
+    
