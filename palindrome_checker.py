1b101b1017a52dcf9998441a364f76b66fec7b0d
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-05 19:05:50 +0530

    Fix output formatting in palindrome_checker.py for consistency in capitalization and spelling.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -2,6 +2,6 @@ word = input("enter a word >")
 word_reverse = word[::-1]
 
 if word == word_reverse:
-    print("the word", word, "is a palindrome!")
+    print("The word", word, "is a palindrome!")
 else:
-    print ("the word", word, "is not a pallindrome!")
+    print ("The word", word, "is not a palindrome!")
