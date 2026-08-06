24496281198d0907dc5bd7bf9b9d9f649e0f6488
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-07 03:33:49 +0530

    Add password_validator.py to prompt the user for a password and validate its strength based on length, digit presence, and uppercase letter requirement, providing feedback on failures.

---
+++ password_validator.py
@@ -0,0 +1,19 @@
+while True:
+   password =input("\nEnter your password ->")
+   failures = []
+
+   if len(password ) < 8:
+    failures.append("Password must contai atleast 8 characters.")
+   if not any(char.isdigit() for char in password):
+     failures.append("Password must contain atleast one digit.")
+   if not any(char.isupper() for char in password):
+    failures.append("Password must contain atleast one uppercase letter.")
+   if failures:
+     for message in failures:
+      print("\n",message)
+   else:
+    print("Password accepted!")
+    break
+
+
+
