b3b0b1b9c3ff8e9070be9655461860197defc5e9
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-07 05:00:06 +0530

    Add fizzbuzz_input.py to implement a FizzBuzz program that prompts the user for a number and prints the FizzBuzz sequence up to that number.

---
+++ fizzbuzz_input.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+for num in range (1, num+1):
+ if num % 3 == 0 and num % 5 == 0:
+    print ("fizzbuzz")
+ elif num % 3 == 0:
+    print ("fizz")
+ elif num % 5 == 0:
+    print ("buzz")
+ else:
+     print (num)
