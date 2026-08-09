4c1ca717a2e2e8e00ee35fe11464142cc64049d6
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-09 19:01:38 +0530

    Add introductory message to Math_quiz.py to enhance user experience and improve prompt formatting for user input.

--- Math_quiz.py
+++ Math_quiz.py
@@ -1,3 +1,5 @@
+print("\nHere's a Math Quiz for you!")
+intro = input("\nEnter 'yes' to start ->")
 import random
 
 score_count = 0
@@ -5,7 +7,7 @@ for i in range(5):
     num1 =random.randint(1,20)
     num2 =random.randint(1,20)
     correct_answer = num1+ num2
-    user_ans =int(input(f"what is {num1} + {num2}? ->1"))
+    user_ans =int(input(f"\nwhat is {num1} + {num2}? ->1"))
     if user_ans == correct_answer:
         score_count = score_count +1
         
