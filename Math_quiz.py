5b4f17d8416402452f8b5fbc94064ec89c651d0b
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-10 18:17:40 +0530

    Update Math_quiz.py to improve user input prompt formatting and enhance score display with clearer output.

--- Math_quiz.py
+++ Math_quiz.py
@@ -7,9 +7,9 @@ for i in range(5):
     num1 =random.randint(1,20)
     num2 =random.randint(1,20)
     correct_answer = num1+ num2
-    user_ans =int(input(f"\nwhat is {num1} + {num2}? ->1"))
+    user_ans =int(input(f"\nwhat is {num1} + {num2}? ->"))
     if user_ans == correct_answer:
         score_count = score_count +1
         
-print ("You scored", score_count, "out of 5!")
+print ("\nYou scored", score_count, "out of 5!")
     
