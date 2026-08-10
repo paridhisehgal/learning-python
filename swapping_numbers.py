ddb45814218257df8316bec73575aa04e886bf5d
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-10 23:28:07 +0530

    Add swapping_numbers.py to implement a simple program that swaps two user-inputted numbers and displays the results before and after the swap.

---
+++ swapping_numbers.py
@@ -0,0 +1,8 @@
+value_a = input("Enter first number: ")
+value_b =input("Enter second number: ")
+
+print("Before swap: a =",value_a,",","b =",value_b)
+
+value_a,value_b = value_b,value_a
+
+print("After swap: a =",value_a,",","b =",value_b)
