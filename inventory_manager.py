b8c46af56023c748e69e0f9e424461d695d3fbbf
paridhisehgal <sehgalparidhi7@gmail.com>
2026-08-11 17:23:33 +0530

    Improve user prompts and messages in inventory_manager.py for clarity and consistency, including adjustments to item addition, insertion, and removal feedback.

--- inventory_manager.py
+++ inventory_manager.py
@@ -1,34 +1,31 @@
-from calendar import c
-
-
 inventory_list=["apple", "bread", "milk", "eggs", "butter"]
 
 while True:
     print("\nMenu:")
-    print("(a) Add item yo end of the list")
+    print("\n(a) Add item to end of the list")
     print("(b)Insert item to a specific location")
     print("(c)Remove item by name")
     print("(d)View sorted inventory")
     print("(e)quit")
 
-    user_input = input("From the above options choose one: ")
+    user_input = input("\nFrom the above options choose one: ")
 
     if user_input == "a":
         item = input("Enter item to add: ")
         inventory_list.append(item)
-        print(item, "added at he end of the list")
+        print(item, "added successfully at the end of the list!")
     
     elif user_input == "b":
-        item = input("Enter item to insert")
+        item = input("Enter item to insert:")
         position = int(input("Enter position (0 = first): "))
         inventory_list.insert(position,item)
-        print(item,"insereted at position", position)
+        print(item,"inserted at position", position)
 
     elif user_input == "c":
         item = input("Enter item to remove:")
         if item in inventory_list:
          inventory_list.remove(item)
-         print(item, "successfully removed from the list")
+         print(item, "successfully removed from the list!")
         else:
             print(item, "is not in the list")
     
